e8cf18de98fda57147bcc1500ee6954b5223f033
Mick Nelson <mickbbnelson@gmail.com>
2021-03-03 13:49:33 -0800

    commit tree 0c9e0a29e74fd889a6e2c9b5bc976b2fbba150e2

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort{|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |word|
+        word[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |awords|
+        awords.start_with?("a")
+    end
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index| 
+        if index == 1
+            element
+        else
+            element + "s"
+        end
+    end
+end
